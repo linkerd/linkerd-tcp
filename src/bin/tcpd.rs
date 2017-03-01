6fd86bd9bb753154a62b02b0c68c32fb3f057813
Oliver Gould <ver@buoyant.io>
2017-03-01 22:35:39 +0000

    Poll namerd for a set of addresses to choose from.
    TODO: load-aware endpoint selection

--- src/bin/tcpd.rs
+++ src/bin/tcpd.rs
@@ -7,13 +7,22 @@ extern crate env_logger;
 extern crate futures;
 extern crate galadriel;
 extern crate getopts;
+#[macro_use]
+extern crate hyper;
 extern crate rand;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 extern crate tokio_core;
+extern crate url;
 
 use futures::{Future, Stream};
+use hyper::Client;
+use hyper::status::StatusCode;
 use rand::{Rng, thread_rng};
+use serde_json as json;
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap};
 use std::rc::Rc;
 use std::env;
 use std::io::{self, Write};
@@ -24,10 +33,15 @@ use std::thread;
 use std::time::Duration;
 use tokio_core::reactor::Core;
 use tokio_core::net::{TcpListener, TcpStream};
+use url::Url;
 
 use galadriel::transfer::BufferedTransfer;
 
 const WINDOW_SIZE: usize = 64 * 1024;
+
+fn stderr(msg: String) {
+    let _ = writeln!(&mut io::stderr(), "{}", msg);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -44,37 +58,50 @@ fn main() {
                     "namerd-addr",
                     "Connect to Namerd's HTTP API on addr:port [default: 127.0.0.1:4180]",
                     "ADDR");
+        opts.optopt("N",
+                    "namerd-ns",
+                    "Namerd namespace [default: default]",
+                    "NS");
+        opts.optopt("i",
+                    "namerd-interval",
+                    "Namerd poll interval, in seconds [default: 60]",
+                    "SECONDS");
         opts
     };
 
-    let matches = opts.parse(&args[1..]).unwrap();
+    let flags = opts.parse(&args[1..]).unwrap();
     drop(env_logger::init());
 
-    let listen_addr = matches.opt_str("listen-addr")
+    let listen_addr = flags.opt_str("listen-addr")
         .unwrap_or("0.0.0.0:7575".to_string())
         .parse()
         .unwrap();
 
-    let namerd_addr = matches.opt_str("namerd-addr")
+    let namerd_addr = flags.opt_str("namerd-addr")
         .unwrap_or("127.0.0.1:4180".to_string())
         .parse()
         .unwrap();
 
+    let namerd_interval = flags.opt_str("namerd-interval")
+        .map(|s| Duration::from_secs(s.parse().unwrap()))
+        .unwrap_or(Duration::from_secs(60));
+
+    let namerd_ns = flags.opt_str("namerd-ns")
+        .unwrap_or("default".to_string());
+
     let target_path;
-    if matches.free.len() == 1 {
-        let path = matches.free[0].clone();
+    if flags.free.len() == 1 {
+        let path = flags.free[0].clone();
         if path.starts_with("/") {
             target_path = path;
         } else {
-            let ref mut err = io::stderr();
-            let _ = writeln!(err, "invalid path: {}", path);
-            print_usage(err, &program, &opts);
+            stderr(format!("invalid path: {}", path));
+            print_usage(&mut io::stderr(), &program, &opts);
             process::exit(64);
         }
     } else {
-        let ref mut err = io::stderr();
-        let _ = writeln!(err, "missing TARGET");
-        print_usage(err, &program, &opts);
+        stderr("missing TARGET".to_string());
+        print_usage(&mut io::stderr(), &program, &opts);
         process::exit(64);
     }
 
@@ -86,8 +113,11 @@ fn main() {
         let listener = TcpListener::bind(&listen_addr, &handle).unwrap();
         info!("Listening on {}", listen_addr);
 
-        let namerd = NamerdLookup::periodic(&namerd_addr, Duration::from_secs(3));
-        info!("Querying namerd for {} on {}", namerd_addr, &target_path);
+        let namerd = NamerdLookup::periodic(&namerd_addr,
+                                            namerd_interval,
+                                            namerd_ns,
+                                            target_path.clone());
+        info!("Querying namerd for {} on {}", namerd_addr, target_path);
 
         let buffer = Rc::new(RefCell::new(vec![0; WINDOW_SIZE]));
         listener.incoming().for_each(move |(up_stream, up_addr)| {
@@ -148,6 +178,7 @@ trait Endpointer {
     fn endpoint(&self) -> Self::Endpoint;
 }
 
+
 #[derive(Debug, PartialEq)]
 struct EndpointState {
     weight: f32,
@@ -156,40 +187,91 @@ struct EndpointState {
 
 struct NamerdLookup {
     is_running: Arc<atomic::AtomicBool>,
-    thread: Rc<RefCell<thread::JoinHandle<()>>>,
+    thread: thread::JoinHandle<()>,
     endpoints: Arc<RwLock<HashMap<SocketAddr, Arc<RwLock<EndpointState>>>>>,
 }
 
 impl NamerdLookup {
-    fn periodic(addr: &SocketAddr, period: Duration) -> NamerdLookup {
+    fn periodic(addr: &SocketAddr,
+                period: Duration,
+                namespace: String,
+                name: String)
+                -> NamerdLookup {
         let is_running = Arc::new(atomic::AtomicBool::new(true));
-        let endpoints = {
-            let mut set = HashMap::new();
-            // FIXME
-            set.insert("127.0.0.1:8080".to_string().parse().unwrap(),
-                       Arc::new(RwLock::new(EndpointState {
-                           weight: 1.0,
-                           load: 0.0,
-                       })));
-            Arc::new(RwLock::new(set))
-        };
+        let endpoints: Arc<RwLock<HashMap<SocketAddr, Arc<RwLock<EndpointState>>>>> =
+            Arc::new(RwLock::new(HashMap::new()));
         let thread = {
             let is_running = is_running.clone();
             let endpoints = endpoints.clone();
             let addr = addr.clone();
+            let url = {
+                let base = format!("http://{}:{}/api/1/resolve/{}",
+                                   addr.ip(),
+                                   addr.port().to_string(),
+                                   namespace);
+                Url::parse_with_params(&base, &[("path", &name)]).unwrap()
+            };
             thread::Builder::new()
                 .name(format!("namerd-{}-{}", addr.ip(), addr.port()).to_string())
-                .spawn(move || while is_running.load(atomic::Ordering::Relaxed) {
-                    debug!("TODO hit the resolve api at {}...", addr);
-                    let eps = endpoints.write().unwrap();
-                    drop(eps);
-                    thread::sleep(period);
+                .spawn(move || {
+                    let client = Client::new();
+                    while is_running.load(atomic::Ordering::Relaxed) {
+                        match client.get(url.clone()).send() {
+                            Err(e) => stderr(format!("Failed to fetch addresses: {}", e)),
+                            Ok(rsp) => {
+                                if rsp.status == StatusCode::Ok {
+                                    let parsed: json::Result<NamerdResponse> =
+                                        json::from_reader(rsp);
+                                    match parsed {
+                                        Err(e) => {
+                                            stderr(format!("Failed to parse response: {}", e))
+                                        }
+                                        Ok(rsp) => {
+                                            // TODO extract weights from meta.
+                                            let addrs = rsp.addrs
+                                                .iter()
+                                                .map(|na| {
+                                                    let ip = na.ip.parse().unwrap();
+                                                    SocketAddr::new(ip, na.port)
+                                                })
+                                                .collect::<HashSet<SocketAddr>>();
+                                            let mut eps = endpoints.write().unwrap();
+                                            let rm_keys = eps.keys()
+                                                .filter_map(|&a| if addrs.contains(&a) {
+                                                    None
+                                                } else {
+                                                    Some(a.clone())
+                                                })
+                                                .collect::<HashSet<SocketAddr>>();
+                                            for k in rm_keys.iter() {
+                                                println!("removing {:?}", k);
+                                                eps.remove(&k);
+                                            }
+                                            for addr in addrs.iter() {
+                                                if !eps.contains_key(&addr) {
+                                                    println!("Adding {}", addr);
+                                                    eps.insert(addr.clone(),
+                                                               Arc::new(RwLock::new(EndpointState {
+                                                                   weight: 1.0,
+                                                                   load: 0.0,
+                                                               })));
+                                                }
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    stderr(format!("Failed to fetch addresses: {}", rsp.status));
+                                }
+                            }
+                        }
+                        thread::sleep(period);
+                    }
                 })
                 .unwrap()
         };
         NamerdLookup {
             is_running: is_running,
-            thread: Rc::new(RefCell::new(thread)),
+            thread: thread,
             endpoints: endpoints,
         }
     }
@@ -199,8 +281,7 @@ impl Drop for NamerdLookup {
     fn drop(&mut self) {
         info!("shutting down namerd");
         self.is_running.store(false, atomic::Ordering::SeqCst);
-        // let mut thread = self.thread.borrow_mut();
-        // thread.join().unwrap();
+        // self.thread.join().unwrap();
     }
 }
 
@@ -215,6 +296,37 @@ impl WithAddr for Endpoint {
     }
 }
 
+impl Endpointer for NamerdLookup {
+    type Endpoint = Endpoint;
+
+    fn endpoint(&self) -> Endpoint {
+        let endpoints = self.endpoints.read().unwrap();
+        let addrs: Vec<(&SocketAddr, &Arc<RwLock<EndpointState>>)> = endpoints.iter().collect();
+
+        // TODO choose an endpoint more intelligently.
+        // E.g.
+        // - if there are more nodes, choose two distinct nodes (at random)
+        // - choose based on weight and load
+        let (addr, state): (&SocketAddr, &Arc<RwLock<EndpointState>>) =
+            *thread_rng().choose(&addrs).unwrap();
+        let addr = addr.clone();
+        let state = (*state).clone();
+
+        // XXX currently we use a simple load metric (# active conns).
+        // This load metric should be pluggable to account for
+        // throughput, etc.
+        {
+            let mut state = state.write().unwrap();
+            state.load += 1.0;
+            debug!("Using {} {:?}", addr, *state);
+        }
+        Endpoint {
+            addr: addr,
+            state: state,
+        }
+    }
+}
+
 impl Drop for Endpoint {
     fn drop(&mut self) {
         let mut state = self.state.write().unwrap();
@@ -223,26 +335,17 @@ impl Drop for Endpoint {
     }
 }
 
-impl Endpointer for NamerdLookup {
-    type Endpoint = Endpoint;
+#[derive(Debug, Deserialize)]
+struct NamerdResponse {
+    #[serde(rename = "type")]
+    kind: String,
+    addrs: Vec<NamerdAddr>,
+    meta: HashMap<String, String>,
+}
 
-    fn endpoint(&self) -> Endpoint {
-        let endpoints = self.endpoints.read().unwrap();
-        let addrs: Vec<(&SocketAddr, &Arc<RwLock<EndpointState>>)> = endpoints.iter().collect();
-        // XXX for now we just choose a node at random.
-        // We should employ some strategy that combines load and weight
-        let (addr, state): (&SocketAddr, &Arc<RwLock<EndpointState>>) =
-            *thread_rng().choose(&addrs).unwrap();
-        let addr = addr.clone();
-        let state = (*state).clone();
-        {
-            let mut state = state.write().unwrap();
-            debug!("Using {} {:?}", addr, *state);
-            state.load += 1.0;
-        }
-        Endpoint {
-            addr: addr,
-            state: state,
-        }
-    }
+#[derive(Debug, Deserialize)]
+struct NamerdAddr {
+    ip: String,
+    port: u16,
+    meta: HashMap<String, String>,
 }
