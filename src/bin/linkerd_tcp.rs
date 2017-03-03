964ddf7439f84e23221fe6e9787f5849ac56ca03
Oliver Gould <ver@buoyant.io>
2017-03-03 00:23:51 +0000

    arg parsing touchup -- move app building into a function

--- src/bin/linkerd_tcp.rs
+++ src/bin/linkerd_tcp.rs
@@ -25,57 +25,33 @@ use linkerd_tcp::namerd;
 
 const WINDOW_SIZE: usize = 64 * 1024;
 
+static LISTEN_ADDR_OPT: &'static str = "listen-addr";
+static NAMERD_ADDR_OPT: &'static str = "namerd-addr";
+static NAMERD_NS_OPT: &'static str = "namerd-ns";
+static NAMERD_INTERVAL_OPT: &'static str = "namerd-interval";
+static TARGET_OPT: &'static str = "TARGET";
+
 fn main() {
+    // Configure the logger from the RUST_LOG environment variable.
     drop(env_logger::init());
 
-    let app = App::new(crate_name!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .arg(Arg::with_name("listen-addr")
-            .short("l")
-            .long("listen-addr")
-            .default_value("0.0.0.0:7575")
-            .takes_value(true)
-            .value_name("ADDR")
-            .validator(is_socket_addr)
-            .help("Accept connections on the given local address and port"))
-        .arg(Arg::with_name("namerd-addr")
-            .short("n")
-            .long("namerd-addr")
-            .default_value("127.0.0.1:4180")
-            .takes_value(true)
-            .value_name("ADDR")
-            .validator(is_socket_addr)
-            .help("The address of namerd's HTTP interface"))
-        .arg(Arg::with_name("namerd-ns")
-            .short("N")
-            .long("namerd-ns")
-            .default_value("default")
-            .takes_value(true)
-            .value_name("NS")
-            .help("Namerd namespace in which the target will be resolved"))
-        .arg(Arg::with_name("namerd-interval")
-            .short("i")
-            .long("namerd-interval")
-            .default_value("60")
-            .takes_value(true)
-            .value_name("SECS")
-            .help("Namerd refresh interval in seconds"))
-        .arg(Arg::with_name("TARGET")
-            .required(true)
-            .index(1)
-            .validator(is_path_like)
-            .help("Destination name (e.g. /svc/foo)"));
-
-    let opts = app.get_matches();
-    let listen_addr = opts.value_of("listen-addr").unwrap().parse().unwrap();
-    let namerd_addr = opts.value_of("namerd-addr").unwrap().parse().unwrap();
-    let namerd_ns = opts.value_of("namerd-ns").unwrap();
+    // Parse and load command-line options.
+    let opts = mk_app().get_matches();
+    let listen_addr = opts.value_of(LISTEN_ADDR_OPT).unwrap().parse().unwrap();
+    let namerd_addr = opts.value_of(NAMERD_ADDR_OPT).unwrap().parse().unwrap();
+    let namerd_ns = opts.value_of(NAMERD_NS_OPT).unwrap();
     let namerd_interval = {
-        let i = opts.value_of("namerd-interval").unwrap().parse().unwrap();
+        let i = opts.value_of(NAMERD_INTERVAL_OPT).unwrap().parse().unwrap();
         Duration::from_secs(i)
     };
-    let target_path = opts.value_of("TARGET").unwrap();
+    let target_path = opts.value_of(TARGET_OPT).unwrap();
+
+    // Currently, we poll namerd to resolve a downstream name. This is
+    // done in a dedicated thread with blocking I/O and sleep. Namerd
+    // exposes a P2C Weighted-Least-Connections balancer.
+    //
+    // This probably should be refactored a bit to separate the namerd
+    // polling from balancing logic.
 
     let balancer = namerd::Endpointer::periodic(&namerd_addr,
                                                 namerd_interval,
@@ -86,14 +62,15 @@ fn main() {
           namerd_addr,
           namerd_interval.as_secs());
 
-    // Create the event loop. All work done in the service of proxying
-    // requests is done on this reactor in the main thread.
-    let mut core = Core::new().unwrap();
-
     // Listen on a socket. For each connection accepted, ask the
     // balancer for a downstream endpoint. Initiate a connection to
     // that endpoint and proxy all data between the upstream and
     // downstream connections, copying data into a temporary buffer.
+    //
+    // All of this happens on the main thread, via tokio's Core
+    // reactor.
+
+    let mut core = Core::new().unwrap();
     let proxy = {
         let handle = core.handle();
         let listener = TcpListener::bind(&listen_addr, &handle).unwrap();
@@ -135,6 +112,48 @@ fn main() {
     };
 
     core.run(proxy).unwrap();
+}
+
+fn mk_app() -> App<'static, 'static> {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .arg(Arg::with_name(LISTEN_ADDR_OPT)
+            .short("l")
+            .long(LISTEN_ADDR_OPT)
+            .default_value("0.0.0.0:7575")
+            .takes_value(true)
+            .value_name("ADDR")
+            .validator(is_socket_addr)
+            .help("Accept connections on the given local address and port"))
+        .arg(Arg::with_name(NAMERD_ADDR_OPT)
+            .short("n")
+            .long(NAMERD_ADDR_OPT)
+            .default_value("127.0.0.1:4180")
+            .takes_value(true)
+            .value_name("ADDR")
+            .validator(is_socket_addr)
+            .help("The address of namerd's HTTP interface"))
+        .arg(Arg::with_name(NAMERD_NS_OPT)
+            .short("N")
+            .long(NAMERD_NS_OPT)
+            .default_value("default")
+            .takes_value(true)
+            .value_name("NS")
+            .help("Namerd namespace in which the target will be resolved"))
+        .arg(Arg::with_name(NAMERD_INTERVAL_OPT)
+            .short("i")
+            .long(NAMERD_INTERVAL_OPT)
+            .default_value("60")
+            .takes_value(true)
+            .value_name("SECS")
+            .help("Namerd refresh interval in seconds"))
+        .arg(Arg::with_name(TARGET_OPT)
+            .required(true)
+            .index(1)
+            .validator(is_path_like)
+            .help("Destination name (e.g. /svc/foo) to be resolved through namerd"))
+
 }
 
 fn is_path_like(v: String) -> Result<(), String> {
