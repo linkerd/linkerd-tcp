b8780cc481375bbd3c7be4680cc220a415345efb
Oliver Gould <ver@buoyant.io>
2017-05-03 23:02:20 +0000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -24,26 +24,60 @@ extern crate tokio_io;
 extern crate tokio_timer;
 extern crate url;
 
+use std::iter::Iterator;
 use std::net;
+use std::rc::Rc;
 
-mod driver;
-pub mod app;
-pub mod lb;
-pub mod namerd;
-
-use driver::Driver;
-pub use lb::Balancer;
+mod namerd;
 
 #[derive(Clone, Debug)]
 pub struct WeightedAddr(pub net::SocketAddr, pub f32);
 
+pub type PathElem = Rc<Vec<u8>>;
+pub type PathElems = Rc<Vec<PathElem>>;
+
 #[derive(Clone, Debug)]
-pub struct Path {
-    elems: std::rc::Rc<Vec<String>>,
+pub struct Path(PathElems);
+impl Path {
+    pub fn empty() -> Path { Path(Rc::new(Vec::new())) }
+
+    pub fn new(elems0: Vec<Vec<u8>>) -> Path {
+        let mut elems = Vec::with_capacity(elems0.len());
+        for el in elems0 {
+            elems.push(Rc::new(el));
+        }
+        Path(Rc::new(elems))
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn elems(&self) -> PathElems {
+        self.0.clone()
+    }
+
+    pub fn concat(&self, other: Path) -> Path {
+        if self.len() == 0 {
+            other.clone()
+        } else if other.len() == 0 {
+            self.clone()
+        } else {
+            let mut elems = Vec::with_capacity(self.len() + other.len());
+            for el in &*self.0 {
+                elems.push(el.clone());
+            }
+            for el in &*other.0 {
+                elems.push(el.clone());
+            }
+            Path(Rc::new(elems))
+        }
+    } 
 }
 
+/// Describes an incoming conneciton.
 #[derive(Clone)]
-pub struct IncomingEnvelope {
+pub struct Envelope {
     srv_addr: net::SocketAddr,
     src_addr: net::SocketAddr,
     src_id: Option<Path>,
@@ -55,11 +89,13 @@ pub struct IncomingEnvelope {
 }
 
 // TODO a r/w stream
-struct DuplexStream {}
+pub enum Socket {}
 
-pub struct IncomingConnection {
-    envelope: IncomingEnvelope,
-    stream: DuplexStream,
+pub struct EnvelopedConnection {
+    pub envelope: Envelope,
+    pub socket: Socket,
 }
 
-pub type Incoming = futures::Stream<Item = IncomingConnection, Error = std::io::Error> + 'static;
+pub type EnvelopedConnectionStream = futures::Stream<Item = EnvelopedConnection,
+                                                     Error = std::io::Error> + 'static;
+                                                     
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,55 @@
-#[macro_use]
+//#[macro_use]
 extern crate clap;
-#[macro_use]
+//#[macro_use]
 extern crate log;
 extern crate pretty_env_logger;
 extern crate linkerd_tcp;
 
-use clap::{Arg, App};
-use linkerd_tcp::app::{self, Runner};
-use std::fs;
-use std::io::Read;
-use std::thread;
+// use clap::{Arg, App};
+// use linkerd_tcp::app::{self, Runner};
+// use std::fs;
+// use std::io::Read;
+// use std::thread;
 
-static CONFIG_PATH_ARG: &'static str = "PATH";
+// static CONFIG_PATH_ARG: &'static str = "PATH";
 
 fn main() {
     // Configure the logger from the RUST_LOG environment variable.
     drop(pretty_env_logger::init());
 
-    // Load command-line options.
-    let opts = App::new(crate_name!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .arg(Arg::with_name(CONFIG_PATH_ARG)
-            .required(true)
-            .index(1)
-            .help("Config file path."))
-        .get_matches();
+    //     // Load command-line options.
+    //     let opts = App::new(crate_name!())
+    //         .version(crate_version!())
+    //         .about(crate_description!())
+    //         .arg(Arg::with_name(CONFIG_PATH_ARG)
+    //             .required(true)
+    //             .index(1)
+    //             .help("Config file path."))
+    //         .get_matches();
 
-    // Parse configuration file.
-    let config = {
-        let path = opts.value_of(CONFIG_PATH_ARG).unwrap();
-        let mut txt = String::new();
-        fs::File::open(path)
-            .and_then(|mut f| f.read_to_string(&mut txt))
-            .expect("failed to read config");
-        app::config::from_str(&txt).expect("configuration error")
-    };
+    //     // Parse configuration file.
+    //     let config = {
+    //         let path = opts.value_of(CONFIG_PATH_ARG).unwrap();
+    //         let mut txt = String::new();
+    //         fs::File::open(path)
+    //             .and_then(|mut f| f.read_to_string(&mut txt))
+    //             .expect("failed to read config");
+    //         app::config::from_str(&txt).expect("configuration error")
+    //     };
 
-    // Process the configuration, splitting it into two threads. These threads are
-    // connected by synchronization primitives as needed, but no work is being done yet.
-    // Next, we'll attach each of these to a reactor in an independent thread, driving
-    // both admin and serving work.
-    let (admin, proxies) = app::configure(config);
+    //     // Process the configuration, splitting it into two threads. These threads are
+    //     // connected by synchronization primitives as needed, but no work is being done yet.
+    //     // Next, we'll attach each of these to a reactor in an independent thread, driving
+    //     // both admin and serving work.
+    //     let (admin, proxies) = app::configure(config);
 
-    // Run admin in a new thread.z
-    let admin_thread = thread::Builder::new()
-        .name("admin".into())
-        .spawn(move || admin.run().expect("could not run admin"))
-        .expect("could not spawn admin thread");
+    //     // Run admin in a new thread.z
+    //     let admin_thread = thread::Builder::new()
+    //         .name("admin".into())
+    //         .spawn(move || admin.run().expect("could not run admin"))
+    //         .expect("could not spawn admin thread");
 
-    proxies.run().expect("could not run proxies");
-    admin_thread.join().expect("admin thread failed to join");
-    info!("Closing.")
+    //     proxies.run().expect("could not run proxies");
+    //     admin_thread.join().expect("admin thread failed to join");
+    //     info!("Closing.")
 }
