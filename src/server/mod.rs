57d6ef64cfe01d55e43e37b275c887cb989fc7fa
Oliver Gould <ver@buoyant.io>
2017-05-18 18:54:08 +0000

    make server::Bound implement a future directly, rather than Sink.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -8,7 +8,7 @@ use std::{io, net};
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::sync::Arc;
-use tokio_core::net::{TcpStream, TcpListener};
+use tokio_core::net::{TcpStream, TcpListener, Incoming};
 use tokio_core::reactor::Handle;
 //use tacho::Scope;
 
@@ -36,35 +36,6 @@ fn unbound(addr: net::SocketAddr,
     Unbound(meta)
 }
 
-pub struct Unbound(Meta);
-impl Unbound {
-    pub fn bind(self, reactor: &Handle) -> io::Result<Bound> {
-        let listen = TcpListener::bind(&self.0.listen_addr, reactor)?;
-        Ok(Bound {
-               reactor: reactor.clone(),
-               listener: listen,
-               meta: self.0,
-           })
-    }
-}
-
-pub struct Bound {
-    reactor: Handle,
-    listener: TcpListener,
-    meta: Meta,
-}
-impl Bound {
-    fn into_future(mut self) -> Box<Future<Item = (), Error = ()>> {
-        let listener = self.listener;
-        let serving = Serving {
-            reactor: self.reactor,
-            meta: self.meta,
-        };
-        let fut = listener.incoming().forward(serving);
-        Box::new(fut.map(|_| {}).map_err(|_| {}))
-    }
-}
-
 struct Meta {
     pub listen_addr: net::SocketAddr,
     pub dst_name: Path,
@@ -74,58 +45,83 @@ struct Meta {
     pub ctx: ServerCtx,
 }
 
-struct Serving {
-    reactor: Handle,
-    meta: Meta,
+pub struct Unbound(Meta);
+impl Unbound {
+    pub fn bind(self, reactor: &Handle) -> io::Result<Bound> {
+        let listen = TcpListener::bind(&self.0.listen_addr, reactor)?;
+        Ok(Bound {
+               reactor: reactor.clone(),
+               bound_addr: listen.local_addr().unwrap(),
+               incoming: listen.incoming(),
+               meta: self.0,
+           })
+    }
 }
 
-impl Sink for Serving {
-    type SinkItem = (TcpStream, net::SocketAddr);
-    type SinkError = io::Error;
-    fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, io::Error> {
-        let dst_name = self.meta.dst_name.clone();
-        let ctx = self.meta.ctx.clone();
-        let buf = self.meta.buf.clone();
-        let router = self.meta.router.clone();
+pub struct Bound {
+    reactor: Handle,
+    incoming: Incoming,
+    meta: Meta,
+    bound_addr: net::SocketAddr,
+}
+impl Future for Bound {
+    type Item = ();
+    type Error = io::Error;
 
-        // Finish accepting the connection from the server.
-        //
-        // TODO we should be able to get metadata from a TLS handshake but we can't!
-        let src = {
-            let (tcp, _) = item;
-            let sock: Box<Future<Item = Socket, Error = io::Error>> =
-                match self.meta.tls.as_ref() {
-                    None => Box::new(future::ok(Socket::plain(tcp))),
-                    Some(tls) => Box::new(Socket::secure_server_handshake(tcp, &tls.config)),
-                };
-            let dst_name = dst_name.clone();
-            sock.map(move |sock| Connection::new(dst_name, sock, ctx))
-        };
+    fn poll(&mut self) -> Poll<(), io::Error> {
+        // Accept all inbound connections from the listener and spawn their work into the
+        // router. This should perhaps yield control back to the reactor periodically.
+        loop {
+            match self.incoming.poll()? {
+                Async::NotReady => {
+                    return Ok(Async::NotReady);
+                }
+                Async::Ready(None) => {
+                    return Ok(Async::Ready(()));
+                }
+                Async::Ready(Some((tcp, _))) => {
+                    // Finish accepting the connection from the server.
+                    //
+                    // TODO we should be able to get metadata from a TLS handshake but we can't!
+                    let src = {
+                        let sock: Box<Future<Item = Socket,
+                                             Error = io::Error>> = match self.meta.tls.as_ref() {
+                            None => Box::new(future::ok(Socket::plain(tcp))),
+                            Some(tls) => {
+                                Box::new(Socket::secure_server_handshake(tcp, &tls.config))
+                            }
+                        };
+                        let dst_name = self.meta.dst_name.clone();
+                        let ctx = self.meta.ctx.clone();
+                        sock.map(move |sock| Connection::new(dst_name, sock, ctx))
+                    };
 
-        // Obtain a balancer.
-        let bal = router.route(&dst_name, &self.reactor);
+                    // Obtain a dispatcher.
+                    let dst = self.meta
+                        .router
+                        .route(&self.meta.dst_name, &self.reactor);
 
-        // Once the incoming connection is ready and we have a balancer ready, obtain an
-        // outbound connection and begin streaming. We obtain an outbound connection after
-        // the incoming handshake is complete so that we don't waste outbound connections
-        // on failed inbound connections.
-        let duplex = src.join(bal)
-            .and_then(move |(src, bal)| {
-                          bal.dispatch()
-                              .and_then(move |dst| Duplex::new(src, dst, buf))
-                      });
+                    // Once the incoming connection is ready and we have a balancer ready, obtain an
+                    // outbound connection and begin streaming. We obtain an outbound connection after
+                    // the incoming handshake is complete so that we don't waste outbound connections
+                    // on failed inbound connections.
+                    let duplex = {
+                        let buf = self.meta.buf.clone();
+                        src.join(dst)
+                            .and_then(move |(src, dst)| {
+                                          dst.dispatch()
+                                              .and_then(move |dst| Duplex::new(src, dst, buf))
+                                      })
+                    };
 
-        // Do all of this work in a single, separate task so that we may process
-        // additional connections while this connection is open.
-        //
-        // TODO: implement some sort of backpressure here?
-        self.reactor.spawn(duplex.map(|_| {}).map_err(|_| {}));
-        Ok(AsyncSink::Ready)
-    }
-
-    fn poll_complete(&mut self) -> Poll<(), io::Error> {
-        // TODO allow servers to be closed gracefully.
-        Ok(Async::NotReady)
+                    // Do all of this work in a single, separate task so that we may process
+                    // additional connections while this connection is open.
+                    //
+                    // TODO: implement some sort of backpressure here?
+                    self.reactor.spawn(duplex.map(|_| {}).map_err(|_| {}));
+                }
+            }
+        }
     }
 }
 
