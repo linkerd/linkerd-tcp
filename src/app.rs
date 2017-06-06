b72c60f9f21cf361470973c2c3f08d814e1a064c
Oliver Gould <ver@buoyant.io>
2017-06-06 00:55:51 +0000

    clear export content on each refresh

--- src/app.rs
+++ src/app.rs
@@ -301,6 +301,7 @@ impl AdminRunner {
                 .for_each(move |_| {
                               let report = reporter.take();
                               let mut export = prometheus.borrow_mut();
+                              export.clear();
                               tacho::prometheus::write(&mut *export, &report)
                                   .expect("error foramtting metrics for prometheus");
                               Ok(())
